1105b6ad633985302435f7f04f354a88e8cb3e50
anibalm <55467466+anibalmisel@users.noreply.github.com>
2024-08-13 15:35:47 -0400

    Se borro linea de codigo en el footer html, se agrego comentarios en css y en el archivo javascript, se ajustaron algunos margenes y height en el css.

--- styles/script.js
+++ styles/script.js
@@ -1,3 +1,4 @@
+//variables
 const texArea = document.getElementById("textarea_a");
 const mensajeEncriptado = document.getElementById("textarea_b");
 const btnCopiar = document.getElementById("btn__copiar");
@@ -9,31 +10,31 @@ const PATTERNS = [
   ['o', 'ober'],
   ['u', 'ufat']
 ];
-function btnEncriptar() {
-  const textoEncriptado = encriptar(texArea.value);
-  mensajeEncriptado.value = textoEncriptado;
-  texArea.value = "";
-  mensajeEncriptado.style.backgroundImage = 'none';
-  return;
-}
+
+//Esta funcion remueve los acentos..
+
 function removeAccents(str) {
   return str.normalize('NFKD').replace(/[\u0300-\u036f]/g, '');
 }
+
+//la funcion encriptar transforma el texto en una cadena plana escrita en minuscula
+//sin acentos, y sin caracteres especiales.
+
 function encriptar(stringEncriptado) {
   stringEncriptado = removeAccents(stringEncriptado);
   stringEncriptado = stringEncriptado.replace(/[^\w\s]/g, '');
   stringEncriptado = stringEncriptado.toLowerCase();
+  /*
+  esta estructura itera sobre el arreglo PATTERNS y,
+  para cada elemento, busca los caracteres en la cadena de texto stringEncriptado
+  utilizando una expresión regular, y los reemplaza con la cadena de texto correspondiente.
+  */
   PATTERNS.forEach(([char, replacement]) => {
     stringEncriptado = stringEncriptado.replace(new RegExp(char, 'g'), replacement);
   });
   return stringEncriptado;
 }
-function btnDesencriptar() {
-  const textoEncriptado = desencriptar(texArea.value);
-  mensajeEncriptado.value = textoEncriptado;
-  texArea.value = "";
-  return;
-}
+
 function desencriptar(stringDesencriptado) {
   stringDesencriptado = stringDesencriptado.toLowerCase();
   PATTERNS.forEach(([char, replacement]) => {
@@ -41,6 +42,25 @@ function desencriptar(stringDesencriptado) {
   });
   return stringDesencriptado;
 }
+
+//Botones...
+
+function btnEncriptar() {
+  const textoEncriptado = encriptar(texArea.value);
+  mensajeEncriptado.value = textoEncriptado;
+  texArea.value = "";
+  mensajeEncriptado.style.backgroundImage = 'none';
+  return;
+}
+
+function btnDesencriptar() {
+  const textoEncriptado = desencriptar(texArea.value);
+  mensajeEncriptado.value = textoEncriptado;
+  texArea.value = "";
+  return;
+}
+
+//Copia el texto ya encriptado.
 function copyText() {
   const textarea = document.getElementById("textarea_b");
   const text = textarea.value;
